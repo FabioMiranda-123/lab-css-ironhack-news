35aa321ef48986b03fd7bc17618e44bab20245b7
FabioMiranda-123 <fabio_rebelo@outlook.pt>
2025-01-19 21:45:50 +0000

    Complete

--- javascript/width-label.js
+++ javascript/width-label.js
@@ -11,3 +11,15 @@ window.addEventListener("resize", () => {
   // Set the text of the element to show the viewport width
   viewportDimensions.innerHTML = `Viewport Width: <b>${width}px</b>`;
 });
+
+
+
+// GOOGLE DID THIS | TEST! 
+const menuIcon = document.querySelector('.menu-icon');
+const navbar = document.querySelector('.navbar');
+
+// Adiciona um evento de clique no botão do menu
+menuIcon.addEventListener('click', () => {
+  // Alterna a classe 'show' na navbar
+  navbar.classList.toggle('show');
+});
